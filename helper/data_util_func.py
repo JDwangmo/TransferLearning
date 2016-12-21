bc8f735e8f31e7a77e9b2865febb4d6b20aa3f5f
jdwang <383287471@qq.com>
2016-12-21 17:35:35 +0800

    update

--- helper/data_util_func.py
+++ helper/data_util_func.py
@@ -7,7 +7,17 @@
 """
 from __future__ import print_function
 import pandas as pd
+import re
 import numpy as np
+import stop_words
+from sklearn.feature_extraction import stop_words as sklearn_stopwords
+
+STOP_WORDS = set(
+    stop_words.get_stop_words('english')
+    + list(sklearn_stopwords.ENGLISH_STOP_WORDS)
+    + ['i', 'why', 'what', 'a']
+)
+print('the numbers of STOP WORDS: %d' % len(STOP_WORDS))
 
 
 def load_data(file_name, header=0, encoding='utf8', index_col=None, converters=None):
@@ -24,7 +34,7 @@ def load_data(file_name, header=0, encoding='utf8', index_col=None, converters=N
                        encoding=encoding,
                        header=header,
                        quoting=0,
-                       # converters=converters,
+                       converters=converters,
                        index_col=index_col,
                        )
     return data
@@ -46,6 +56,22 @@ def save_data(data, file_name, header=True, index=False):
                 index=index,
                 header=header,
                 encoding='utf8')
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+def clean_text(text):
+    # 全部转为小写
+    text = text.lower()
+    # 去掉html字符
+    text = re.sub('<.*?>', ' ', text)
+    # 去掉数字
+    # text = re.sub('\d+', ' ', text)
+    # 去掉非字母等字符
+    text = re.sub('[^a-z\'-]', ' ', text)
+    # 将多空白符全部 转为 单空格
+    text = re.sub('\s+', ' ', text)
+    # 去除stop words ,且长度大于1
+    text = [item for item in text.strip().split(' ') if item not in STOP_WORDS and len(item)>1]
+    return text
 
 
 def show_df_info(data=None):
